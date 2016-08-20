a3bac13096b1582e5a437a68b9d9fcc58d5bc0fa
Chandralinjo <boris.majic@gmail.com>
2016-08-20 02:44:29 +0200

    First working version
    Suprised that RK4 misses the orbit so much, probably due to mistakes in code :)
    NOTE: Code messy, survived alot of debugging, next version should be clear

--- rk4.py
+++ rk4.py
@@ -6,6 +6,7 @@ Created on Fri Aug 19 18:41:08 2016
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 import os
 
@@ -13,7 +14,7 @@ class body:
     position = np.zeros([3,1])
     velocity = np.zeros([3,1])
     acc = np.zeros([3,1])
-    mass = 0
+    mass = 0.0
     
     positionStep = np.zeros([4,3])
     velocityStep = np.zeros([4,3])
@@ -26,36 +27,106 @@ class body:
         self.velocity = v
         self.mass = m
       
-def radius(body1, body2):
-    r = math.sqrt(np.sum(np.square(body1.position - body2.position)))
+#Distance between two body objects:
+def bodyDistance(body1, body2):
+    r = np.sum(np.square(body1.position - body2.position)) ** 0.5
     return(r)
-    
+
+#Distance between two 3-radius vectors (in form of numpy arrays):
+def distance(vector1, vector2):
+    r = np.sum(np.square(vector1 - vector2)) ** 0.5
+    return r
+
 G = 6.67e-11                #Gravitational constant
 secInYr = 365.24*24*3600    #Seconds in a year
-dt = 86400.0                  #Time step (in seconds)
-maxTime = secInYr * 10      #Simulation time (in seconds)
-AU = 149597871000.0           #Astronomical unit (in meters)       
+stepSize = 86400.0                  #Time step (in seconds)
+maxTime = secInYr    #Simulation time (in seconds)
+#maxTime = 80000.0
+AU = 1.49597871000e11           #Astronomical unit (in meters)       
+
+x = []
+y = []
        
 #Setting up Sun - Earth system for testing
 #TODO: Set up initialization from file
 bodies = []
-bodies.append(body(np.array([AU, 0, 0]), np.array([0, 2*math.pi*AU/(secInYr), 0]), 6e24))
-bodies.append(body(np.array([0, 0, 0]), np.array([0, 0, 0]), 2e30))
+bodies.append(body(np.array([AU, 0, 0], dtype = float), np.array([0, 2*math.pi*AU/(secInYr), 0], dtype = float), 6.0e24))
+bodies.append(body(np.array([0, 0, 0], dtype = float), np.array([0, 0, 0], dtype = float), 2.0e30))
 N = len(bodies)
 
 currTime = 0
 while currTime < maxTime:
-    #Calculating acceleration (second derivative ov position)
+    #Calculating step 1 accelerations (second derivative ov position)
+    for i in range(N):
+        bodies[i].positionStep = np.zeros([4,3])
+        bodies[i].velocityStep = np.zeros([4,3])        
+        bodies[i].accStep = np.zeros([4,3])        
+        Force = np.zeros([1,3], dtype = float)
+        for j in range(N):
+            if(i != j):
+                Force += -G * bodies[i].mass * bodies[j].mass * (bodies[i].position - bodies[j].position) / (bodyDistance(bodies[i], bodies[j]) ** 3)
+        bodies[i].accStep[0] = Force / bodies[i].mass
+        
+        
+    #Calculating step 1 velocities and positions:
+    for i in range(N):
+        bodies[i].velocityStep[0] += bodies[i].accStep[0]
+        bodies[i].positionStep[0] += bodies[i].velocity
+        
+    
+    #Calculating step 2 accelerations:
+    for i in range(N):
+        Force = np.zeros([1,3], dtype = float)
+        for j in range(N):
+            if(i != j):
+                Force += -G* bodies[i].mass * bodies[j].mass * ((bodies[i].position + bodies[i].positionStep[0] * stepSize / 2) - (bodies[j].position + bodies[j].positionStep[0] * stepSize / 2)) / (distance(bodies[i].position + bodies[i].positionStep[0] * stepSize / 2, bodies[j].position + bodies[j].positionStep[0] * stepSize / 2) ** 3)
+        bodies[i].accStep[1] = Force / bodies[i].mass        
+        #print(i, bodies[i].accStep)
+        #print('\n')
+    #Calculating step 2 velocities and positions:
+    for i in range(N):
+        bodies[i].velocityStep[1] += bodies[i].accStep[1]
+        bodies[i].positionStep[1] += bodies[i].velocity + bodies[i].velocityStep[0] * stepSize / 2
+        print(bodies[i].positionStep)
+   
+   #Calculating step 3 accelerations:
+    for i in range(N):
+        Force = np.zeros([1,3], dtype = float)
+        for j in range(N):
+            if(i != j):
+               Force += -G* bodies[i].mass * bodies[j].mass * ((bodies[i].position + bodies[i].positionStep[1] * stepSize / 2) - (bodies[j].position + bodies[j].positionStep[1] * stepSize / 2)) / (distance(bodies[i].position + bodies[i].positionStep[1] * stepSize / 2, bodies[j].position + bodies[j].positionStep[1] * stepSize / 2) ** 3)
+        bodies[i].accStep[2] = Force / bodies[i].mass
+    
+    #Calculating step 3 velocities and positions:    
+    for i in range(N):
+        bodies[i].velocityStep[2] += bodies[i].accStep[2]
+        bodies[i].positionStep[2] += bodies[i].velocity + bodies[i].velocityStep[1] * stepSize / 2
+    #Calculating step 4 accelerations:
     for i in range(N):
         Force = np.zeros([1,3])
         for j in range(N):
             if(i != j):
-                Force += -G * bodies[i].mass * bodies[j].mass * (bodies[i].position - bodies[j].position) / (radius(bodies[i], bodies[j]) ** 3)
-        bodies[i].acc = Force / bodies[i].mass
-        
+                Force += -G* bodies[i].mass * bodies[j].mass * ((bodies[i].position + bodies[i].positionStep[2] * stepSize / 2) - (bodies[j].position + bodies[j].positionStep[2] * stepSize / 2)) / (distance(bodies[i].position + bodies[i].positionStep[2] * stepSize / 2, bodies[j].position + bodies[j].positionStep[2] * stepSize / 2) ** 3)
+        bodies[i].accStep[3] = Force / bodies[i].mass
+        #print(i, bodies[i].accStep)
     
-    currTime += dt
+    #Calculating step 4 velocities and positions:    
+    for i in range(N):
+        bodies[i].velocityStep[3] += bodies[i].accStep[3]
+        bodies[i].positionStep[3] += bodies[i].velocity + bodies[i].velocityStep[1] * stepSize
+
+    #Calculating next itteration velocities and positions   
+    for i in range(N):    
+        bodies[i].velocity += stepSize / 6 * (bodies[i].velocityStep[0] + 2 * bodies[i].velocityStep[1] + 2 * bodies[i].velocityStep[2] + bodies[i].velocityStep[3])
+        bodies[i].position += stepSize / 6 * (bodies[i].positionStep[0] + 2 * bodies[i].positionStep[1] + 2 * bodies[i].positionStep[2] + bodies[i].positionStep[3])
     
+    x.append(bodies[0].position[0])
+    y.append(bodies[0].position[1])
+    
+    currTime += stepSize
+
+plt.scatter(x,y)
+plt.show()
     
     
     
